85d4a6079fb80ea0318b8446821dc79d8b07bd2d
Derek Walton <90069173+Dewed02@users.noreply.github.com>
2023-03-23 19:30:51 -0600

    Update db.ts

--- src/db/db.ts
+++ src/db/db.ts
@@ -13,6 +13,7 @@ CREATE TABLE Users (
 CREATE TABLE Messages (
     id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
     recipient INTEGER NOT NULL REFERENCES Users(id),
+    sender TINYTEXT NOT NULL,
     data TEXT NOT NULL,
     secureMessage TEXT NOT NULL
 );
